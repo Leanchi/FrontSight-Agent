3c5d984288a2c1553faecf6b5e01ad28ce30a4ce
刘凌志 <8023139+LeanchiLiu@user.noreply.gitee.com>
2026-05-13 10:13:47 +0800

    fix github action

--- meshcore/paintbrush_overlay.h
+++ meshcore/paintbrush_overlay.h
@@ -1,7 +1,7 @@
 #ifndef __PAINTBRUSH_OVERLAY__
 #define __PAINTBRUSH_OVERLAY__
 
-#include "duktape.h"
+#include "../microscript/duktape.h"
 
 void paintbrush_overlay_PUSH(duk_context *ctx, void *chain);
 
--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Apr-23 00:40:56+0800"
-#define SOURCE_COMMIT_HASH "4cd7cfaedf27143515a2b6d4c99b5606d0b2b8d0"
+#define SOURCE_COMMIT_DATE "2026-May-13 10:08:14+0800"
+#define SOURCE_COMMIT_HASH "e73dd49e3755dc873d31dd477c8477d2ec32f4f2"
